5d8776053b2abfc3916df901c3f5bad73ba93d39
jwokaty <jwokaty@users.noreply.github.com>
2020-09-15 23:39:39 -0400

    Make created_by, modified_by not required fixes #9

--- common_files/models/timestamp.py
+++ common_files/models/timestamp.py
@@ -8,11 +8,11 @@ class Timestamp(models.Model):
     """Abstract base class Timestamp model has created and modified fields"""
 
     created = models.DateTimeField(auto_now_add=True)
-    created_by = models.ForeignKey(User, on_delete=models.CASCADE,
-                                   related_name='+')
+    created_by = models.ForeignKey(User, blank=True, null=True,
+                                   on_delete=models.CASCADE, related_name='+')
     modified = models.DateTimeField(auto_now=True)
-    modified_by = models.ForeignKey(User, on_delete=models.CASCADE,
-                                    related_name='+')
+    modified_by = models.ForeignKey(User, blank=True, null=True,
+                                    on_delete=models.CASCADE, related_name='+')
 
     class Meta:
         abstract = True
