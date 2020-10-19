afbab076b5fd7558796163f3e9adb86aa30f0246
jwokaty <jwokaty@users.noreply.github.com>
2020-10-19 18:29:21 -0400

    Add test for TimestampCreateView's custom form_valid fixes #10

--- common_files/tests/__init__.py
+++ common_files/tests/__init__.py
@@ -1,8 +1,9 @@
 """CommonFiles tests"""
 
+from common_files.tests.active_tabular_inline import ActiveTabularInlineTest
 from common_files.tests.base import BaseAdminTest
 from common_files.tests.start_time import StartTimeListFilterTest
 from common_files.tests.timestamp import TimestampAdminTest
 from common_files.tests.timestamp_active import TimestampActiveAdminTest
+from common_files.tests.timestamp_create_view import TimestampCreateViewTest
 from common_files.tests.utils import CommonFilesUtilsTest
-from common_files.tests.active_tabular_inline import ActiveTabularInlineTest
